8a228d4a5e6262f24609fb16212755910b96cdf6
s22012 <s22012@std.it-college.ac.jp>
2023-10-11 09:38:02 +0900

    viewを過去のサンプルと同じように再現できた

--- KtorSample/app/build.gradle.kts
+++ KtorSample/app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
 }
