0a71029f25504e7efdffd9763aa49b28a6deb870
s22012 <s22012@std.it-college.ac.jp>
2023-10-17 11:29:39 +0900

    chap13 background完了

---
+++ ServiceSample/app/src/main/java/jp/ac/it_college/std/s22012/servicesample/SoundManageService.kt
@@ -0,0 +1,45 @@
+package jp.ac.it_college.std.s22012.servicesample
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.IBinder
+
+class SoundManageService : Service() {
+    private var mediaPlayer: MediaPlayer? = null
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onCreate() {
+        mediaPlayer = MediaPlayer()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val mediaFileUri = Uri.parse(
+            "android.resource://${packageName}/${R.raw.cheen}"
+        )
+        mediaPlayer?.run {
+            setDataSource(this@SoundManageService, mediaFileUri)
+            setOnPreparedListener{ onMediaPlayerPrepared()}
+            setOnCompletionListener { onPlaybackEnd() }
+            prepareAsync()
+        }
+        return START_NOT_STICKY
+    }
+    override fun onDestroy() {
+        mediaPlayer?.run {
+            if (isPlaying) {
+                stop()
+            }
+        }
+        super.onDestroy()
+    }
+    private fun onMediaPlayerPrepared() {
+        mediaPlayer?.start()
+    }
+    private fun onPlaybackEnd(){
+        stopSelf()
+    }
+}
