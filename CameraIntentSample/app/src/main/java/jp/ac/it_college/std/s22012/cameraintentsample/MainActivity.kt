dfe205424550b04940a7b1cd49c9b117b3e2e2ac
s22012 <s22012@std.it-college.ac.jp>
2023-10-24 12:26:46 +0900

    chap15 finish

--- CameraIntentSample/app/src/main/java/jp/ac/it_college/std/s22012/cameraintentsample/MainActivity.kt
+++ CameraIntentSample/app/src/main/java/jp/ac/it_college/std/s22012/cameraintentsample/MainActivity.kt
@@ -1,11 +1,50 @@
 package jp.ac.it_college.std.s22012.cameraintentsample
 
+import android.content.ContentValues
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import jp.ac.it_college.std.s22012.cameraintentsample.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var imageUri: Uri? = null
+    private val cameraLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result?.resultCode == RESULT_OK) {
+            binding.iVCamera.setImageURI(imageUri)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iVCamera.setOnClickListener{ onCameraImageClick() }
+    }
+    private fun onCameraImageClick() {
+        val timestamp = SimpleDateFormat("yyyyMMddHHmmss", Locale.ROOT).format(Date())
+        val fileName = "Photo_${timestamp}.jpg"
+        val values = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE,"image/jpg")
+            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraIntentSample")
+        }
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        }
+        cameraLauncher.launch(intent)
     }
 }
