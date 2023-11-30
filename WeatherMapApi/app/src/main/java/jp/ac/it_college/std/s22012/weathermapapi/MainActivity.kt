d59034088b429cd605db89ef4b098ee349c277e6
itc-s22012 <104543144+itc-s22012@users.noreply.github.com>
2023-11-30 11:37:06 +0900

    Update MainActivity.kt

--- WeatherMapApi/app/src/main/java/jp/ac/it_college/std/s22012/weathermapapi/MainActivity.kt
+++ WeatherMapApi/app/src/main/java/jp/ac/it_college/std/s22012/weathermapapi/MainActivity.kt
@@ -17,15 +17,15 @@ class MainActivity : AppCompatActivity() {
 
         val listView: ListView = findViewById(R.id.cityListView)
 
-        // 都市データのリストから都市名のみを抽出して ArrayAdapter に設定
+       
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, cityList.map { it.name })
         listView.adapter = adapter
 
-        // リストアイテムがクリックされたときの処理
+     
         listView.setOnItemClickListener { _, _, position, _ ->
             val selectedCity = cityList[position]
 
-            // WeatherDetailActivity に選択された都市のIDを渡して起動
+         
             val intent = Intent(this, WeatherDetailActivity::class.java).apply {
                 putExtra("EXTRA_SELECTED_CITY_ID", selectedCity.locationId)
             }
